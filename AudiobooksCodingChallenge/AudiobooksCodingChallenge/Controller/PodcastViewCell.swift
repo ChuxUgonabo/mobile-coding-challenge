ad44c2bf21f3b3a0001c71dd4ac150db71a517bf
John_Ugonabo <chuxugonabo@gmail.com>
2023-04-27 12:51:05 -0700

    Added a table view for screen 1 and set appropriate constraints, also added a custom cell and xib file including neccesary properties

---
+++ AudiobooksCodingChallenge/AudiobooksCodingChallenge/Controller/PodcastViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PodcastViewCell.swift
+//  AudiobooksCodingChallenge
+//
+//  Created by Chux Ugonabo MacBook on 2023-04-26.
+//
+
+import UIKit
+
+class PodcastViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var podcastThumbnailImageView: UIImageView!
+    @IBOutlet weak var podcastTitleLabel: UILabel!
+    @IBOutlet weak var podcastPublisherNameLabel: UILabel!
+    @IBOutlet weak var podcastFavouriteStateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
