c10545c5ddc03e7c9b087e8d6cce382377ad2421
John_Ugonabo <chuxugonabo@gmail.com>
2023-04-27 15:17:33 -0700

    Added a PodcastItemViewController

---
+++ AudiobooksCodingChallenge/AudiobooksCodingChallenge/Controller/PodcastItemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PodcastItemViewController.swift
+//  AudiobooksCodingChallenge
+//
+//  Created by Chux Ugonabo MacBook on 2023-04-27.
+//
+
+import UIKit
+
+class PodcastItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
