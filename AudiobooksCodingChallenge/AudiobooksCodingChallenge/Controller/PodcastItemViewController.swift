55785b880f07868558bdd80e2028edd94474ef85
John_Ugonabo <chuxugonabo@gmail.com>
2023-04-27 17:43:55 -0700

    Connected outlets in PodcastItemViewController

--- AudiobooksCodingChallenge/AudiobooksCodingChallenge/Controller/PodcastItemViewController.swift
+++ AudiobooksCodingChallenge/AudiobooksCodingChallenge/Controller/PodcastItemViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class PodcastItemViewController: UIViewController {
 
+    @IBOutlet weak var podcastTitleLabel: UILabel!
+    @IBOutlet weak var podcastPublisherNameLabel: UILabel!
+    @IBOutlet weak var podcastThumbnailImageView: UIImageView!
+    @IBOutlet weak var podcastDescriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func backButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func favouriteButtonAction(_ sender: UIButton) {
+    }
 }
